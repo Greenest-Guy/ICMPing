2b9d8a1ccd3b9537af8781e191d35fcd075869ee
Greenest-Guy <greenestcaleb@gmail.com>
2025-08-19 01:12:10 -0400

    PEP8 formatting

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from datetime import datetime
 from customtkinter import *
 from icmplib import ping
 from PIL import Image
-import ipaddress, os, time, threading, sys
+import ipaddress
+import os
+import time
+import threading
+import sys
 
 '''
 ICMPing - A lightweight tool used for sending ICMP (ping) requests.
@@ -16,12 +20,12 @@ All code written by Caleb Greene
 '''
 
 
-class ErrorWindow(CTkToplevel): # Errow Window class - Inherits from CTkToplevel
-    def __init__(self, parent, message): 
+class ErrorWindow(CTkToplevel):  # Errow Window class - Inherits from CTkToplevel
+    def __init__(self, parent, message):
         super().__init__()
         self.geometry("250x75")
         self.title("ERROR")
-        self.resizable(False, False) # disables resizing of the window
+        self.resizable(False, False)  # disables resizing of the window
 
         self.transient(parent)  # Make this window always on top of parent
         self.grab_set()         # Block interaction with parent window until closed
@@ -29,7 +33,8 @@ class ErrorWindow(CTkToplevel): # Errow Window class - Inherits from CTkToplevel
         self.label = CTkLabel(master=self, text=message, wraplength=225)
         self.label.pack(pady=3, padx=20)
 
-        self.close_button = CTkButton(master=self, text="Close", corner_radius=32, fg_color="#029cff", hover_color="#0062b1", command=self.destroy)
+        self.close_button = CTkButton(master=self, text="Close", corner_radius=32,
+                                      fg_color="#029cff", hover_color="#0062b1", command=self.destroy)
         self.close_button.pack(padx=20, pady=10)
 
 
@@ -39,39 +44,31 @@ class Data():
         self.sent = 0
         self.received = 0
         self.rtts = []
-    
 
     def increment_sent(self):
         self.sent += 1
 
-
     def increment_received(self):
         self.received += 1
-
 
     def get_current_time():
         current_time = datetime.now().time()
         return current_time.strftime("%H:%M:%S")
-    
 
-    def get_current_time_dot(): # returns current time seperated with . for file naming
+    def get_current_time_dot():  # returns current time seperated with . for file naming
         current_time = datetime.now().time()
         return current_time.strftime("%H.%M.%S")
-
 
     def get_current_date():
         current_date = datetime.now().date()
         return current_date.strftime("%Y-%m-%d")
 
-
     def no_reply(self):
         return f"[{self.sent}] {Data.get_current_time()} - No reply from {self.ip}\n"
 
-
     def add_rtt(self, rtt):
-        self.rtts.append(rtt)  
+        self.rtts.append(rtt)
         return f"[{self.sent}] {Data.get_current_time()} - Reply from {self.ip} RTT = {rtt}ms\n"
-
 
     def summary(self, interval, timeout):
         message = (
@@ -79,8 +76,8 @@ class Data():
             f"Packets Sent: {self.sent}\n"
             f"Packets Received: {self.received}\n"
             f"Packet Loss: {((self.sent - self.received) / self.sent) * 100:.2f}%\n\n"
-            )
-        
+        )
+
         if interval == 1:
             message += f"Interval: {interval} second\n"
         else:
@@ -90,90 +87,102 @@ class Data():
             message += f"Timeout: {timeout} second\n\n"
         else:
             message += f"Timeout: {timeout} seconds\n\n"
-        
+
         if self.sent > 1 and self.received > 1:
             message += (
-            f"RTT Min: {round(min(self.rtts), 3) if self.rtts else 'N/A'}ms\n"
-            f"RTT Max: {round(max(self.rtts), 3) if self.rtts else 'N/A'}ms\n"
-            f"RTT Avg: {round(sum(self.rtts) / len(self.rtts), 3) if self.rtts else 'N/A'}ms\n"
-            f"Jitter: {round(max(self.rtts) - min(self.rtts), 3) if self.rtts else 'N/A'}ms\n\n"
+                f"RTT Min: {round(min(self.rtts), 3) if self.rtts else 'N/A'}ms\n"
+                f"RTT Max: {round(max(self.rtts), 3) if self.rtts else 'N/A'}ms\n"
+                f"RTT Avg: {round(sum(self.rtts) / len(self.rtts), 3) if self.rtts else 'N/A'}ms\n"
+                f"Jitter: {round(max(self.rtts) - min(self.rtts), 3) if self.rtts else 'N/A'}ms\n\n"
             )
         return message
 
 
-class ICMPing(CTk): # Main class - Inherits from CTk
+class ICMPing(CTk):  # Main class - Inherits from CTk
     def __init__(self):
         super().__init__()
         set_appearance_mode("dark")
         self.geometry("750x250")
         self.title("ICMPing")
-        self.resizable(False, False) # Disable resizing of the main window
+        self.resizable(False, False)  # Disable resizing of the main window
 
-        current_dir = os.path.dirname(os.path.abspath(__file__)) # Get the current path of the code
+        # Get the current path of the code
+        current_dir = os.path.dirname(os.path.abspath(__file__))
 
         bg_path = os.path.join(current_dir, "BG.png")
         dbutton_path = os.path.join(current_dir, "dButton.png")
 
-        if sys.platform == "win32":
+        operating_system = sys.platform
+
+        if operating_system == "win32":
             icon_path = os.path.join(current_dir, "logo.ico")
             self.iconbitmap(icon_path)
 
-        elif sys.platform == "darwin":
+        elif operating_system == "darwin":
             try:
-                self.iconphoto(True, PhotoImage(file=os.path.join(current_dir, "logo.png")))
-            
+                self.iconphoto(True, PhotoImage(
+                    file=os.path.join(current_dir, "logo.png")))
+
             except Exception as e:
                 pass
 
         # Images
         bg_image = Image.open(bg_path)
-        background = CTkImage(light_image=bg_image, dark_image=bg_image, size=(750, 250))
+        background = CTkImage(light_image=bg_image,
+                              dark_image=bg_image, size=(750, 250))
         image_label = CTkLabel(self, image=background, text="")
         image_label.place(x=0, y=0)
 
         download_image = Image.open(dbutton_path)
-        download_button = CTkImage(light_image=download_image, dark_image=download_image, size=(25, 25))
+        download_button = CTkImage(
+            light_image=download_image, dark_image=download_image, size=(25, 25))
 
         # Ping Count input
-        self.Countinput = CTkEntry(master=self, width=125, text_color="#ffffff", bg_color="#2f2f2f", corner_radius=0, placeholder_text="Number")
+        self.Countinput = CTkEntry(master=self, width=125, text_color="#ffffff",
+                                   bg_color="#2f2f2f", corner_radius=0, placeholder_text="Number")
         self.Countinput.place(x=180, y=73, anchor="center")
 
         # Interval input
-        self.Intervalinput = CTkEntry(master=self, width=125, text_color="#ffffff", bg_color="#2f2f2f", corner_radius=0, placeholder_text="Seconds")
+        self.Intervalinput = CTkEntry(master=self, width=125, text_color="#ffffff",
+                                      bg_color="#2f2f2f", corner_radius=0, placeholder_text="Seconds")
         self.Intervalinput.place(x=180, y=108, anchor="center")
 
         # Timeout input
-        self.Timeoutinput = CTkEntry(master=self, width=125, text_color="#ffffff", bg_color="#2f2f2f", corner_radius=0, placeholder_text="Seconds")
+        self.Timeoutinput = CTkEntry(master=self, width=125, text_color="#ffffff",
+                                     bg_color="#2f2f2f", corner_radius=0, placeholder_text="Seconds")
         self.Timeoutinput.place(x=180, y=143, anchor="center")
 
         # IP address input
-        self.IPinput = CTkEntry(master=self, width=125, text_color="#ffffff", bg_color="#2f2f2f", corner_radius=0, placeholder_text="IPv4 or IPv6")
+        self.IPinput = CTkEntry(master=self, width=125, text_color="#ffffff",
+                                bg_color="#2f2f2f", corner_radius=0, placeholder_text="IPv4 or IPv6")
         self.IPinput.place(x=180, y=178, anchor="center")
 
         # Ping button
-        self.button = CTkButton(master=self, width=125, height=25, text="Start Ping", corner_radius=16, fg_color="#029cff", hover_color="#0062b1", command=self.ping, font=("", 16), bg_color="#2f2f2f")
+        self.button = CTkButton(master=self, width=125, height=25, text="Start Ping", corner_radius=16,
+                                fg_color="#029cff", hover_color="#0062b1", command=self.ping, font=("", 16), bg_color="#2f2f2f")
         self.button.place(x=180, y=225, anchor="center")
 
         # Download button
-        self.Downloadbutton = CTkButton(master=self, image=download_button, command=self.download, width=25, height=25, text="", bg_color="#2f2f2f", fg_color="#2f2f2f", hover=False)
+        self.Downloadbutton = CTkButton(master=self, image=download_button, command=self.download,
+                                        width=25, height=25, text="", bg_color="#2f2f2f", fg_color="#2f2f2f", hover=False)
         self.Downloadbutton.place(x=25, y=225, anchor="center")
 
-        self.textbox = CTkTextbox(master=self, width=500, height=250, bg_color="#1c1c1c", text_color="#ffffff")
+        self.textbox = CTkTextbox(
+            master=self, width=500, height=250, bg_color="#1c1c1c", text_color="#ffffff")
 
         self.ErrorWindow = None
 
-
     def validIP(self):
         try:
-            ipaddress.ip_address(self.IPinput.get()) # Validate the IP address using ipaddress
+            # Validate the IP address using ipaddress
+            ipaddress.ip_address(self.IPinput.get())
             return True
         except ValueError:
             return False
 
-
     def open_error_window(self, message):
 
-        if self.ErrorWindow is None or not self.ErrorWindow.winfo_exists(): # Check if the window is not already open
+        if self.ErrorWindow is None or not self.ErrorWindow.winfo_exists():  # Check if the window is not already open
             self.ErrorWindow = ErrorWindow(self, message)
 
         self.update_idletasks()
@@ -193,45 +202,47 @@ class ICMPing(CTk): # Main class - Inherits from CTk
 
         self.enable_button()
         self.ErrorWindow.geometry(f"{error_width}x{error_height}+{x}+{y}")
-        self.ErrorWindow.focus() # Bring window to the front
-    
+        self.ErrorWindow.focus()  # Bring window to the front
 
     def disable_button(self):
         self.button.configure(state="disabled")
-    
 
     def enable_button(self):
         self.button.configure(state="normal")
-
 
     def ping_logic(self):
         # Validation
         if not self.validIP():
             self.open_error_window("Invalid IP address.")
             return
-        
-        try: # Validates Count, Interval, and Timeout inputs
-            count = int(self.Countinput.get()) if self.Countinput.get() != "" else 1 # default of 1 ping
-            interval = int(self.Intervalinput.get()) if self.Intervalinput.get() != "" else 1 # default of 1 second interval
-            timeout = int(self.Timeoutinput.get()) if self.Timeoutinput.get() != "" else 2 # default of 2 seconds timeout
-            
+
+        try:  # Validates Count, Interval, and Timeout inputs
+            count = int(self.Countinput.get()) if self.Countinput.get(
+            ) != "" else 1  # default of 1 ping
+            interval = int(self.Intervalinput.get()) if self.Intervalinput.get(
+            ) != "" else 1  # default of 1 second interval
+            timeout = int(self.Timeoutinput.get()) if self.Timeoutinput.get(
+            ) != "" else 2  # default of 2 seconds timeout
+
         except ValueError:
-            self.open_error_window("Count, Interval, and Timeout must be integers.")
+            self.open_error_window(
+                "Count, Interval, and Timeout must be integers.")
             return
-        
+
         if count < 1:
             self.open_error_window("Count must be at least 1.")
             return
-        
+
         if interval < 0 or timeout < 0:
-            self.open_error_window("Interval and Timeout must be non-negative integers.")
-            return
-        
-        if count >= sys.maxsize or interval >= sys.maxsize or timeout >= sys.maxsize:
-            self.open_error_window("Count, Interval, and Timeout must be less than the maximum integer size.")
+            self.open_error_window(
+                "Interval and Timeout must be non-negative integers.")
             return
 
-        
+        if count >= sys.maxsize or interval >= sys.maxsize or timeout >= sys.maxsize:
+            self.open_error_window(
+                "Count, Interval, and Timeout must be less than the maximum integer size.")
+            return
+
         # Ping
         self.data = Data(self.IPinput.get())
 
@@ -239,50 +250,48 @@ class ICMPing(CTk): # Main class - Inherits from CTk
 
         for i in range(count):
             try:
-                response = ping(self.IPinput.get(), count=1, timeout=timeout, privileged=False) # Send a ping request
+                # Send a ping request
+                response = ping(self.IPinput.get(), count=1,
+                                timeout=timeout, privileged=False)
             except Exception as e:
                 pass
 
             if response.is_alive:
-                self.data.increment_sent();self.data.increment_received()
+                self.data.increment_sent()
+                self.data.increment_received()
                 self.add(response.avg_rtt)
                 self.data.add_rtt(response.avg_rtt)
             else:
                 self.data.increment_sent()
                 self.display(self.data.no_reply())
-                
 
             time.sleep(interval)
 
         self.display(self.data.summary(interval, timeout))
         self.enable_button()
 
-
     def ping(self):
         thread = threading.Thread(target=self.ping_logic)
         self.disable_button()
-        thread.daemon = True 
+        thread.daemon = True
         thread.start()
-        
 
     def display(self, text):
         self.textbox.after(0, lambda: self.textbox.insert("end", text))
 
-
     def add(self, rtt):
-        self.textbox.after(0, lambda: self.textbox.insert("end", self.data.add_rtt(rtt)))
-    
+        self.textbox.after(0, lambda: self.textbox.insert(
+            "end", self.data.add_rtt(rtt)))
 
     def display_textbox(self):
         self.textbox.place(relx=2/3, rely=1/2, anchor="center")
-
 
     def download(self):
         folder_path = filedialog.askdirectory()
 
         if not folder_path:
             return  # User clicked cancel
-        
+
         file_name = f"ICMPing {Data.get_current_date()} {Data.get_current_time_dot()}.txt"
         file = os.path.join(folder_path, file_name)
 
